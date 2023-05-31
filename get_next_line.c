fdfe971111d40faa92634eac96d8554e8b04ee06
Marta Diez Asensio <114190534+MartaDiezAsensio@users.noreply.github.com>
2023-05-31 14:23:19 +0200

    Now working

---
+++ get_next_line.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 13:41:35 by mdiez-as          #+#    #+#             */
+/*   Updated: 2023/05/31 14:21:42 by mdiez-as         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = ft_read(fd, str);
+	if (!str)
+		return (NULL);
+	line = ft_getline(str);
+	str = ft_getmem(str);
+	return (line);
+}
+
+//Read the first line of a fd
+char	*ft_read(int fd, char *str)
+{
+	char	*buffer;
+	int		bytes;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	//Itera mientras los bytes leídos no sean 0 y no se encuentre '\n' en la str.
+	while (bytes != 0 && !ft_strchr(str, '\n'))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		str = ft_strjoin(str, buffer);
+	}
+	free(buffer);
+	return(str);
+}
+
+//Del buffer leído, retorna la primera línea
+
+char	*ft_getline(char *full_str)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!full_str[i])
+		return (NULL);
+	while (full_str[i] && full_str[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (full_str[i] && full_str[i] != '\n')
+	{
+		line[i] = full_str[i];
+		i++;
+	}
+	if (full_str[i] == '\n')
+	{
+		line[i] = full_str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char *ft_getmem(char *str)
+{
+	int		i;
+	int		j;
+	char	*mem;
+
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (NULL);
+	}
+	mem = (char *)malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!mem)
+		return (NULL);
+	i++;
+	j = 0;
+	while (str[i] != '\0')
+		mem[j++] = str[i++];
+	mem[j] = '\0';
+	free(str);
+	return (mem);
+}
---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 13:40:24 by mdiez-as          #+#    #+#             */
+/*   Updated: 2023/05/31 14:21:19 by mdiez-as         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_read(int fd, char *str);
+char	*ft_getline(char *str);
+char	*ft_getmem(char *str);
+
+#endif
